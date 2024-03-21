12ae5706e2e9d9b4548542284e4f7deccc845c39
40624987 <40624987@live.napier.ac.uk>
2024-03-21 10:59:28 +0000

    automated version

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -188,15 +188,25 @@ public class App
      */
     public void printSalaries(ArrayList<Employee> employees)
     {
+        if (employees == null){
+            System.out.println("Null ArrayList");
+        }
+        else{
         // Print header
         System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
         // Loop over all employees in the list
         for (Employee emp : employees)
         {
+            if(emp == null){
+                System.out.println("Null Employee");
+            }
+            else{
             String emp_string =
                     String.format("%-10s %-15s %-20s %-8s",
                             emp.emp_no, emp.first_name, emp.last_name, emp.salary);
             System.out.println(emp_string);
+                }
+            }
         }
     }
 }
